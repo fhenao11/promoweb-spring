92f23f6b1ec43797d8e19077578aa2cfdfdf8a65
unknown <fhenao@gmail.com>
2022-08-10 17:52:54 -0400

    First commit

---
+++ src/main/java/com/example/promowebspring/PromowebSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.promowebspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromowebSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromowebSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/promowebspring/PromowebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.promowebspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromowebSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
