d277c9151cfff2f7a2b7a09a4e424adbdd339e4a
unknown <fhenao@gmail.com>
2022-08-10 17:57:44 -0400

    Second Commit

--- src/main/java/com/example/promowebspring/PromowebSpringApplication.java
+++ src/main/java/com/example/promowebspring/PromowebSpringApplication.java
@@ -2,9 +2,17 @@ package com.example.promowebspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PromowebSpringApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "First Spring Azure web app";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PromowebSpringApplication.class, args);
